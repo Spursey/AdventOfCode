cac0d29059ceec78d1bc3038122da4feee65d031
Steve Pursey <stephenpursey@gmail.com>
2024-11-18 01:23:16 -0800

    Complete 2019 Day 1

---
+++ 2019/Day 01/p1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+
+
+int main(void) {
+	int current_number = 0;
+	int adjusted_number = 0;
+	int total = 0;
+
+  ifstream input_file;
+  string current_line;
+
+	input_file.open("input.txt");
+
+  while(input_file >> current_line) {
+		current_number = stoi(current_line);
+
+		adjusted_number = floor(current_number / 3) - 2;
+
+		total += adjusted_number;
+	}
+
+  printf(" ---------- TOTAL ----------");
+  printf("%i\n", total);
+}
