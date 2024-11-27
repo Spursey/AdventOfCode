ada98dd28d823609587ebc981be558e45c10a1ce
Steve Pursey <stephenpursey@gmail.com>
2024-11-26 22:48:27 -0800

    Practice on some old Advent

---
+++ 2019/Day 01/p2/p2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	int current_number = 0;
+	int adjusted_number = 0;
+	int total = 0;
+
+  ifstream input_file;
+  string current_line;
+
+	input_file.open("input.txt");
+
+  while(input_file >> current_line) {
+		current_number = stoi(current_line);
+
+		adjusted_number = floor(current_number / 3) - 2;
+    
+    total = total + adjusted_number;
+
+    while((floor(adjusted_number / 3) - 2) > 0) {
+      total = total + floor(adjusted_number / 3) - 2;
+
+      adjusted_number = floor(adjusted_number / 3) - 2;
+    }
+
+    adjusted_number = 0;
+	}
+
+  printf(" ---------- TOTAL ----------");
+  printf("%i\n", total);
+}
